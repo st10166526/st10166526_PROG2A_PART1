877a8856a9e5fe55fd083441af5a0c10bc59a0c8
ST10166526_AidanCauston <st10166526@vcconnect.edu.za>
2025-04-23 10:22:05 +0200

    Initial commit - full CyberSecurityBot code

--- obj/Debug/net9.0/CyberSecurityBot.AssemblyInfo.cs
+++ obj/Debug/net9.0/CyberSecurityBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c152f5a88428c2a0fc6f00e4ee87195007f555b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CyberSecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
